5ef9b37fbb84160e09e322d7cfc5918717626d5c
Alexandre de O. Zamberlan <alexz@ufn.edu.br>
2024-04-22 17:44:54 -0300

    material para aulas do final de semana

---
+++ 1_numpy_pandas/5_pandas_arquivo.py
@@ -0,0 +1,11 @@
+import pandas
+
+#carregar arquivo em dataframe
+
+#exibir dataframe
+
+#exibir fatias do dataframe
+
+#limpar dados ou corrigir dados
+
+#aplicar as medidas centrais: media, mediana e moda
